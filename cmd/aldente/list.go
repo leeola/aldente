79a7b55fce723be3228956268b7f9321753d69d6
Lee Olayvar <leeolayvar@gmail.com>
2017-06-06 07:31:29 -0700

    feat: sort machines

--- cmd/aldente/list.go
+++ cmd/aldente/list.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 	"text/tabwriter"
 
+	"github.com/bradfitz/slice"
 	ald "github.com/leeola/aldente"
 	"github.com/leeola/aldente/autoload"
 	"github.com/urfave/cli"
@@ -28,7 +29,6 @@ func ListCmd(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	sort.Strings(groups)
 
 	return listMachines(a, groups)
 }
@@ -37,12 +37,18 @@ func listMachines(a *ald.Aldente, groups []string) error {
 	w := tabwriter.NewWriter(os.Stdout, 2, 0, 2, ' ', 0)
 	fmt.Fprintln(w, "\tGROUP\tNAME\tPROVIDER")
 
+	sort.Strings(groups)
+
 	var i int
 	for _, group := range groups {
 		machines, err := a.GroupMachines(group)
 		if err != nil {
 			return err
 		}
+
+		slice.Sort(machines[:], func(i, j int) bool {
+			return machines[i].Name < machines[j].Name
+		})
 
 		for _, m := range machines {
 			i++
