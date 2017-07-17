ce5b643b70bb1af9ca7bfec281814a7c5929090f
Lee Olayvar <leeolayvar@gmail.com>
2017-07-17 08:35:27 -0700

    feat: added machine api
    .. again.

--- provider.go
+++ provider.go
@@ -52,6 +52,8 @@ type Provider interface {
 	// machine when it was created.
 	Command(io.Writer, MachineRecord, CommandConfig) error
 
+	Machine(MachineRecord) (Machine, error)
+
 	// Provision based on the Provider implementation and configuration.
 	//
 	// Configuration is done via the toml config. Eg, if you want a large aws
