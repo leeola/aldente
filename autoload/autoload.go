0bcfa69f44cb42e7639b45e6868e628512f34ad9
Lee Olayvar <leeola@fastmail.com>
2017-12-08 11:19:40 -0800

    feat: enable db autload

--- autoload/autoload.go
+++ autoload/autoload.go
@@ -4,8 +4,8 @@ import (
 	"github.com/leeola/motley"
 
 	"github.com/leeola/motley/autoload/registry"
-	// _ "github.com/leeola/motley/databases/marshaldb/regloader"
 	_ "github.com/leeola/motley/connectors/local/regloader"
+	_ "github.com/leeola/motley/databases/marshaldb/regloader"
 )
 
 func Motley(configPaths []string) (motley.Motley, error) {
