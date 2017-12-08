4d7a63203b4c4a0c9079b8bc2b2c7141bb9ebbd2
Lee Olayvar <leeola@fastmail.com>
2017-12-08 08:45:44 -0800

    feat: added embed package

---
+++ motley/embed/embed.go
@@ -0,0 +1,60 @@
+package embed
+
+import (
+	"errors"
+
+	"github.com/leeola/motley"
+)
+
+type Config struct {
+	DB         motley.Database
+	Connectors []motley.Connector
+	Providers  []motley.Provider
+}
+
+type Motley struct {
+	config Config
+	db     motley.Database
+
+	providers  map[string]motley.Provider
+	connectors map[string]motley.Connector
+}
+
+func New(c Config) (*Motley, error) {
+	if c.DB == nil {
+		return nil, errors.New("missing required config: DB")
+	}
+
+	if len(c.Providers) == 0 {
+		return nil, errors.New("missing required config: Providers")
+	}
+
+	providersMap := map[string]motley.Provider{}
+	for _, p := range c.Providers {
+		n := p.Name()
+
+		if _, exists := providersMap[n]; exists {
+			return nil, errors.New("duplicate provider name configured")
+		}
+
+		providersMap[n] = p
+	}
+
+	connectorsMap := map[string]motley.Connector{}
+	for _, p := range c.Connectors {
+		n := p.Name()
+
+		if _, exists := providersMap[n]; exists {
+			return nil, errors.New("duplicate provider name configured")
+		}
+
+		connectorsMap[n] = p
+	}
+
+	return &Motley{
+		config:     c,
+		db:         c.DB,
+		providers:  providersMap,
+		connectors: connectorsMap,
+	}, nil
+}
