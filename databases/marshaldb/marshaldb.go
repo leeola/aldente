e38954c8688f6a25a03ac37f489885b61987d493
Lee Olayvar <leeola@fastmail.com>
2018-01-04 19:25:23 -0800

    feat: make marshaldb fit new database interface

--- databases/marshaldb/marshaldb.go
+++ databases/marshaldb/marshaldb.go
@@ -6,14 +6,17 @@ import (
 	"os"
 	"path/filepath"
 
-	ald "github.com/leeola/motley"
 	"github.com/leeola/errors"
+	ald "github.com/leeola/motley"
 )
 
 // data is the core data structure written to json on the fs.
 type data struct {
-	Groups map[string]map[string]ald.MachineRecord
+	Groups map[string]archData
 }
+
+type archData map[string]machineReplicates
+type machineReplicates map[int]ald.MachineRecord
 
 type Config struct {
 	Path string `toml:"path"`
@@ -50,7 +53,7 @@ func (db *MarshalDb) load() (data, error) {
 	// If it's eof, create a new map and return that.
 	if err == io.EOF {
 		return data{
-			Groups: map[string]map[string]ald.MachineRecord{},
+			Groups: map[string]archData{},
 		}, nil
 	}
 
@@ -92,91 +95,120 @@ func (db *MarshalDb) Groups() ([]string, error) {
 	return s, nil
 }
 
-func (db *MarshalDb) GroupMachines(group string) ([]ald.MachineRecord, error) {
-	d, err := db.load()
-	if err != nil {
-		return nil, err
-	}
+// func (db *MarshalDb) GroupMachines(group string) ([]ald.MachineRecord, error) {
+// 	d, err := db.load()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	ms, ok := d.Groups[group]
+// 	if !ok {
+// 		return nil, errors.Errorf("group not found: %s", group)
+// 	}
+//
+// 	// build a slice from the map
+// 	s := make([]ald.MachineRecord, len(ms))
+// 	var i int
+// 	for _, m := range ms {
+// 		s[i] = m
+// 		i++
+// 	}
+//
+// 	return s, nil
+// }
 
-	ms, ok := d.Groups[group]
-	if !ok {
-		return nil, errors.Errorf("group not found: %s", group)
-	}
+//func (db *MarshalDb) CreateGroup(group string, machines []ald.MachineConfig) error {
+//	d, err := db.load()
+//	if err != nil {
+//		return err
+//	}
+//
+//	if _, ok := d.Groups[group]; ok {
+//		return errors.Errorf("group already exists: %s", group)
+//	}
+//
+//	ms := map[string]ald.MachineRecord{}
+//	for _, m := range machines {
+//		if m.Name == "" {
+//			return errors.New("missing required field: Name")
+//		}
+//		if m.Provider == "" {
+//			return errors.New("missing required field: Provider")
+//		}
+//
+//		// ensure no duplicate names
+//		if _, ok := ms[m.Name]; ok {
+//			return errors.Errorf("duplicate machine name: %s", m.Name)
+//		}
+//
+//		ms[m.Name] = ald.MachineRecord{
+//			Name:     m.Name,
+//			Group:    group,
+//			Provider: m.Provider,
+//		}
+//	}
+//
+// 	d.Groups[group] = ms
+// 	return db.save(d)
+// }
 
-	// build a slice from the map
-	s := make([]ald.MachineRecord, len(ms))
-	var i int
-	for _, m := range ms {
-		s[i] = m
-		i++
-	}
+// func (db *MarshalDb) UpdateMachine(m ald.MachineRecord) (bool, error) {
+// 	if m.Name == "" {
+// 		return errors.New("missing required field: Name")
+// 	}
+// 	if m.Group == "" {
+// 		return errors.New("missing required field: Group")
+// 	}
+// 	if m.Provider == "" {
+// 		return errors.New("missing required field: Provider")
+// 	}
+//
+// 	d, err := db.load()
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	ms, ok := d.Groups[m.Group]
+// 	if !ok {
+// 		return errors.Errorf("group not found: %s", m.Group)
+// 	}
+//
+// 	// only update a machine if it exists in the group.
+// 	// Ie, it may not have existed in the config when the group was created.
+// 	if _, ok := ms[m.Name]; !ok {
+// 		return errors.Errorf("group does not contain record for machine: %s", m.Name)
+// 	}
+//
+// 	// update the record, and then save the struct
+// 	ms[m.Name] = m
+// 	return db.save(d)
+//
+// }
 
-	return s, nil
-}
+func (db *MarshalDb) UpsertMachine(m ald.MachineRecord) (bool, error) {
+	// if m.Name == "" {
+	// 	return errors.New("missing required field: Name")
+	// }
+	// if m.Group == "" {
+	// 	return errors.New("missing required field: Group")
+	// }
+	// if m.Provider == "" && m.Connection == "" {
+	// 	return errors.New("missing required field: Provider or Connection")
+	// }
 
-func (db *MarshalDb) CreateGroup(group string, machines []ald.MachineConfig) error {
-	d, err := db.load()
-	if err != nil {
-		return err
-	}
+	// d, err := db.load()
+	// if err != nil {
+	// 	return err
+	// }
 
-	if _, ok := d.Groups[group]; ok {
-		return errors.Errorf("group already exists: %s", group)
-	}
+	// reps, ok := d.Groups[m.Group]
+	// if !ok {
+	// 	reps = map[int]ald.MachineRecord{}
+	// }
 
-	ms := map[string]ald.MachineRecord{}
-	for _, m := range machines {
-		if m.Name == "" {
-			return errors.New("missing required field: Name")
-		}
-		if m.Provider == "" {
-			return errors.New("missing required field: Provider")
-		}
+	// // if _, exists := reps[m.Index]; exists {
+	// // 	return false, nil
+	// // }
 
-		// ensure no duplicate names
-		if _, ok := ms[m.Name]; ok {
-			return errors.Errorf("duplicate machine name: %s", m.Name)
-		}
-
-		ms[m.Name] = ald.MachineRecord{
-			Name:     m.Name,
-			Group:    group,
-			Provider: m.Provider,
-		}
-	}
-
-	d.Groups[group] = ms
-	return db.save(d)
-}
-
-func (db *MarshalDb) UpdateMachine(m ald.MachineRecord) error {
-	if m.Name == "" {
-		return errors.New("missing required field: Name")
-	}
-	if m.Group == "" {
-		return errors.New("missing required field: Group")
-	}
-	if m.Provider == "" {
-		return errors.New("missing required field: Provider")
-	}
-
-	d, err := db.load()
-	if err != nil {
-		return err
-	}
-
-	ms, ok := d.Groups[m.Group]
-	if !ok {
-		return errors.Errorf("group not found: %s", m.Group)
-	}
-
-	// only update a machine if it exists in the group.
-	// Ie, it may not have existed in the config when the group was created.
-	if _, ok := ms[m.Name]; !ok {
-		return errors.Errorf("group does not contain record for machine: %s", m.Name)
-	}
-
-	// update the record, and then save the struct
-	ms[m.Name] = m
-	return db.save(d)
+	return false, errors.New("not implemented")
 }
