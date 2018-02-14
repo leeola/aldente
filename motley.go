8616e6c35d5d0cf27c699a4a5cca6fbdd820ec8a
Lee Olayvar <leeola@fastmail.com>
2018-02-14 15:52:19 -0800

    refactor: tweak motley api

--- motley.go
+++ motley.go
@@ -1,6 +1,9 @@
 package motley
 
+import "io"
+
 type Motley interface {
+	UseConfig(...io.Reader) (hash string, err error)
 	Status(groupName string) (Status, error)
 }
 
--- motley/embed/embed.go
+++ motley/embed/embed.go
@@ -3,6 +3,7 @@ package embed
 import (
 	"errors"
 	"fmt"
+	"io"
 
 	"github.com/leeola/motley"
 )
@@ -10,22 +11,12 @@ import (
 type Config struct {
 	DB         motley.Database
 	Connectors []motley.Connector
-	Providers  []motley.Provider
-
-	ArchitectureConfigs []motley.ArchitectureConfig
-
-	MachineConfigs []motley.MachineConfig
 }
 
 type Motley struct {
-	config Config
-	db     motley.Database
+	db motley.Database
 
-	providers  map[string]motley.Provider
 	connectors map[string]motley.Connector
-
-	archConfigs    map[string]motley.ArchitectureConfig
-	machineConfigs map[string]motley.MachineConfig
 }
 
 func New(c Config) (*Motley, error) {
@@ -33,53 +24,20 @@ func New(c Config) (*Motley, error) {
 		return nil, errors.New("missing required config: DB")
 	}
 
-	providersMap := map[string]motley.Provider{}
-	for _, p := range c.Providers {
-		n := p.Name()
-
-		if _, exists := providersMap[n]; exists {
-			return nil, errors.New("duplicate provider name configured")
-		}
-
-		providersMap[n] = p
-	}
-
 	connectorsMap := map[string]motley.Connector{}
 	for _, p := range c.Connectors {
 		n := p.Name()
 
-		if _, exists := providersMap[n]; exists {
+		if _, exists := connectorsMap[n]; exists {
 			return nil, errors.New("duplicate connector name configured")
 		}
 
 		connectorsMap[n] = p
 	}
 
-	archConfigs := map[string]motley.ArchitectureConfig{}
-	for _, ac := range c.ArchitectureConfigs {
-		if _, exists := archConfigs[ac.Machine]; exists {
-			return nil, errors.New("duplicate architecture name configured")
-		}
-
-		archConfigs[ac.Machine] = ac
-	}
-
-	machineConfigs := map[string]motley.MachineConfig{}
-	for _, mc := range c.MachineConfigs {
-		if _, exists := machineConfigs[mc.Name]; exists {
-			return nil, errors.New("duplicate machine name configured")
-		}
-
-		machineConfigs[mc.Name] = mc
-	}
-
 	mot := &Motley{
-		config:         c,
-		db:             c.DB,
-		providers:      providersMap,
-		connectors:     connectorsMap,
-		archConfigs:    archConfigs,
-		machineConfigs: machineConfigs,
+		db:         c.DB,
+		connectors: connectorsMap,
 	}
 
 	if err := mot.init(); err != nil {
@@ -89,9 +47,14 @@ func New(c Config) (*Motley, error) {
 	return mot, nil
 }
 
+// init actual machines from the given machineConfigs
 func (m *Motley) init() error {
 	// TODO(leeola): upsert all of the machine configs
 	return nil
+}
+
+func (m *Motley) UseConfig(rs ...io.Reader) error {
+	return errors.New("not implemented")
 }
 
 func (m *Motley) Status(groupName string) (motley.Status, error) {
